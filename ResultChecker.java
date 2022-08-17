f59a66a0ca4b8af875467e76958c928e05c75aa7
Jose <64102720+Jose-Rodolfo-Llave@users.noreply.github.com>
2022-08-17 01:14:10 -0500

    Create ResultChecker.java

---
+++ ResultChecker.java
@@ -0,0 +1,13 @@
+public class ResultChecker {
+
+	protected Boolean [] resultados;
+
+	public Boolean[] getResultados() {
+		return resultados;
+	}
+
+	public void setResultados(Boolean[] resultados) {
+		this.resultados = resultados;
+	}
+	
+}
